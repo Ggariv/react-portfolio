2295c81720a4f41eb4963f1cc7b05f58cda23461
Gonzalo <ggarciarivera09@gmail.com>
2023-02-26 11:44:03 -0500

    change

--- myportfolio/src/components/Footer/index.js
+++ myportfolio/src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
                 <a href="https://www.linkedin.com/in/gonzalogarciariv92/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
             </div>
             <p className='footer-info'>
-            &copy; Gonzalo Garcia  |  <a href="../assets/gonzalo-resume.pdf" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+            &copy; Gonzalo Garcia  |  <a href="https://github.com/Ggariv/react-portfolio/blob/main/myportfolio/src/assets/gonzalo-resume.pdf" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
             </p>
         </div>
         )
--- myportfolio/src/components/Resume/index.js
+++ myportfolio/src/components/Resume/index.js
@@ -8,7 +8,7 @@ function Resume() {
                 <div className="mt-5 pl-5 pr-5">
                 Experienced multitasker with solid record of accomplishment in several departments, strategies and key projects. Experienced in financial reporting, project management, market research, deepening partnerships and boosting sales with creative and proactive approaches. Excellent reputation for strategic planning, problems solution, building long-term customer relationships and improving customer satisfaction. Enthusiastic for undertaking new challenges to grow, learn and adapt to new industries.
                 <p className="mt-5"><a href="https://www.linkedin.com/in/gonzalogarciariv92/"><img src="https://img.icons8.com/color/48/000000/linkedin-2.png" alt="LinkedIn"/></a></p>
-                <p>Download my full <a href="../assets/gonzalo-resume.pdf" className="link">resume</a></p>
+                <p>Download my full <a href="https://github.com/Ggariv/react-portfolio/blob/main/myportfolio/src/assets/gonzalo-resume.pdf" className="link">resume</a></p>
                 </div>
             </div>
 
